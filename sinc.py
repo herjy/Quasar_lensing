4cf7de85260c8a5333b0a52141c7238548647e3f
Fatima Zaidouni <zaidouni@gallia.astro.princeton.edu>
2019-06-26 14:33:08 -0400

    gradient descent initial attempt

--- sinc.py
+++ sinc.py
@@ -1,19 +1,23 @@
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
+import pickle
 
 t_s = np.array(np.loadtxt('t_s.txt'))
-t_k = np.array(np.loadtxt('t_k.txt'))
+with open('t_k_.pickle', 'rb') as g:
+     t_k_ = pickle.load(g)
+t_k = t_k_[0]
+h = t_k_[1]
 f_s = np.array(np.loadtxt('f.txt'))  # f(t_s)
 
 
-h = 3.0017678255745435
+#h = 3.0017678255745435
 matrix = t_s[np.newaxis,:]-t_k[:,np.newaxis]
 matrix_sinc = np.sinc(matrix/h)
 transp = matrix_sinc.T
 #print(transp)
 #print(np.linalg.inv(transp))
-f_k_T = np.linalg.lstsq(transp, f_s.T,rcond=-1)[0]
+f_k_T = np.linalg.lstsq(transp, f_s.T,rcond=-1)[0] # uses least squares
 f_k = f_k_T.T # since its one dimensional, this should do nothing
 #print(f_k,f_k_T)
 
@@ -35,3 +39,8 @@ plt.show()
 # change sampling, det(A.T @ A)-1
 # gradient descent to minimize Y-AX
 # take realistic time delays between 5 and 15 for ex
+np.savetxt('matrix_sinc.txt',matrix_sinc)
+np.savetxt('f_s.txt',f_s)
+np.savetxt('f_k.txt',f_k)
+print('f_k shape',f_s.shape)
+print('f_k shape',f_k.shape)
