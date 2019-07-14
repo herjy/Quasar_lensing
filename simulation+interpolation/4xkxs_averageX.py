5442ca43377208fdf615c5c2e8ad9da130906f4d
fzaidouni <fzaidouni@github.com>
2019-07-14 17:58:46 -0400

    update 4ks

--- simulation+interpolation/4xkxs_averageX.py
+++ simulation+interpolation/4xkxs_averageX.py
@@ -7,101 +7,93 @@ t_drive_new = np.loadtxt('t_drive_new.txt') # true time sample.
 ind = index(15,t_drive_new) # 15 means that the time delay is chosen randomly between 0 and 15 days
 
 # getting the indices of each time shifter curve
-index0 = ind[0]
-index1 = ind[1]
-index2 = ind[2]
-index3 = ind[3]
+index0, index1,index2,index3 = ind[0],ind[1],ind[2],ind[3]
 sample = np.sort(ind[5])
 
 #getting the time sample of each curve
-ts_0 = sample[index0]
-ts_1 = sample[index1]
-ts_2 = sample[index2]
-ts_3 = sample[index3]
-ts = np.array([ts_0,ts_1,ts_2,ts_3])
+ts = np.array([sample[index0],sample[index1],sample[index2],sample[index3]])
+
 
 # finding the magnitudes corresponding to each time shifted sample
 f = simulation_(sample) #running simulation
-fs_0 = f[index0]
-fs_1 = f[index1]
-fs_2 = f[index2]
-fs_3 = f[index3]
+fs = np.array([f[index0],f[index1],f[index2],f[index3]])
 
 # desired sampling for each curve
-tk_0 = T_K(3,ts_0)[0]
-tk_1 = T_K(3,ts_1)[0]
-tk_2 = T_K(3,ts_2)[0]
-tk_3 = T_K(3,ts_3)[0]
-tk = np.array([tk_0,tk_1,tk_2,tk_3])
+tk = np.array([T_K(3,ts[0])[0],T_K(3,ts[1])[0],T_K(3,ts[2])[0],T_K(3,ts[3])[0]])
 
 h=3
 time_delays = ind[6]
 
-# sinc matrix  # make this into for loop
-a = np.array([(ts_0[:,np.newaxis]+time_delays[0])-tk_0[np.newaxis,:]])
-b = np.array([(ts_1[:,np.newaxis]+time_delays[1])-tk_1[np.newaxis,:]])
-c = np.array([(ts_2[:,np.newaxis]+time_delays[2])-tk_2[np.newaxis,:]])
-d = np.array([(ts_3[:,np.newaxis]+time_delays[3])-tk_3[np.newaxis,:]])
-sinc_matrix = np.sinc(np.concatenate([a,b,c,d],axis =0)/h)
-k = len(tk_0)
-s = len(ts_0)
+# sinc matrix
+matrix = []
+for i in range(4):
+    matrix.append(np.array([(ts[i][:,np.newaxis]+time_delays[i])-tk[i][np.newaxis,:]]))
+matrix = np.array(matrix)
 
-#D = np.reshape(sinc_matrix,(k))
+A = np.sinc(np.concatenate([matrix[0],matrix[1],matrix[2],matrix[3]],axis =0)/h)
 
-#sinc_matrix.shape # 4 x s x k
+Y= np.concatenate([fs[0][np.newaxis,:],fs[1][np.newaxis,:],fs[2][np.newaxis,:],fs[3][np.newaxis,:]])
 
-B= np.concatenate([fs_0[np.newaxis,:],fs_1[np.newaxis,:],fs_2[np.newaxis,:],fs_3[np.newaxis,:]])
+def AT(X,A,Y): # X = Y-A@X
+    gradient =[]
+    for i in range(4):
+        gradient.append(np.dot(A[i].T, Y[i] - A[i] @ X))
+    gradient = np.array(gradient)
+    return np.mean(gradient)
 
-A = sinc_matrix
-Y = B
+X = np.zeros(len(tk[0]))+12
 
-mu = 0.0038  # just to not get stuck
-
-X = np.zeros(len(tk_0))
-
-count = 0
-R = [np.sum(Y ** 2)]
-epsilon = 0.3
-
-# l = (np.dot(D,Y- np.dot(A,X)))@B
-# print(l.shape)
-
-
-
-while (R[-1] > epsilon) and (
-        count < 10000):  # calculated this norm^2 for the least squares sol it gave 0.5 so I chose 0.5>0.3
-    X_new1 = X + mu * ((A[0].T) @ (Y[0] - A[0] @ X))
-    #X = X_new.copy()
-    X_new2 = X + mu * ((A[1].T) @ (Y[1] - A[1] @ X))
-    #X = X_new.copy()
-    X_new3 = X + mu * ((A[2].T) @ (Y[2] - A[2] @ X))
-    #X = X_new.copy()
-    if count == 80:
-        print(X_new3-X_new2)
-    X_new4 = X + mu * ((A[3].T) @ (Y[3] - A[3] @ X))
-    #X = X_new.copy()
-    X = (np.array(X_new1)+np.array(X_new2)+np.array(X_new3)+np.array(X_new4))/4
-    R.append(np.sum((Y - A @ X) ** 2.))
-    count += 1
-
-
-plt.title('Convergence')
-plt.plot(np.array(R[1:]))
-plt.xlabel('iterations')
-plt.ylabel('error')
-plt.show()
-
-cut = np.logical_and(X < np.max(Y), X > np.min(Y))
-plt.plot(ts_0, Y[0], 'o', label='light curve 1')
-plt.plot(ts_1, Y[1], 'o', label='light curve 2')
-plt.plot(ts_2, Y[2], 'o', label='light curve 3')
-plt.plot(ts_3, Y[3], 'o', label='light curve 4')
-
-plt.plot(tk[0][cut], X[cut], label='interpolated', color='black')
-plt.xlabel('time[days]')
-plt.ylabel('magnitude')
-plt.title('sinc interpolated values')
-plt.legend()
-plt.show()
-
-#print(B[1].shape)
+print(AT(X,A,Y))
+#
+# def linorm(S, nit):
+#     n1, n2 = np.shape(S)
+#     x0 = np.random.rand(1, n1)
+#     x0 = x0 / np.sqrt(np.sum(x0 ** 2))
+#     for i in range(nit):
+#         x = np.dot(x0, S)
+#         xn = np.sqrt(np.sum(x ** 2))
+#         xp = x / xn
+#         y = np.dot(xp, S.T)
+#         yn = np.sqrt(np.sum(y ** 2))
+#         if yn < np.dot(y, x0.T):
+#             break
+#         x0 = y / yn
+#     return 1. / xn
+#
+# mu = 0.0038  # just to not get stuck
+#
+# X = np.zeros(len(tk[0]))
+#
+# count = 0
+# R = [np.sum(Y ** 2)]
+# epsilon = 0.3
+#
+#
+#
+# while (R[-1] > epsilon) and (
+#         count < 5000):  # calculated this norm^2 for the least squares sol it gave 0.5 so I chose 0.5>0.3
+#     X_new = X + mu * AT(Y-A@X)
+#     X = X_new.copy()
+#     R.append(np.sum((Y - A @ X) ** 2.))
+#     count += 1
+#
+#
+# plt.title('Convergence')
+# plt.plot(np.array(R[1:]))
+# plt.xlabel('iterations')
+# plt.ylabel('error')
+# plt.show()
+#
+# cut = np.logical_and(X < np.max(Y), X > np.min(Y))
+# plt.plot(ts_0, Y[0], 'o', label='light curve 1')
+# plt.plot(ts_1, Y[1], 'o', label='light curve 2')
+# plt.plot(ts_2, Y[2], 'o', label='light curve 3')
+# plt.plot(ts_3, Y[3], 'o', label='light curve 4')
+#
+# plt.plot(tk[0][cut], X[cut], label='interpolated', color='black')
+# plt.xlabel('time[days]')
+# plt.ylabel('magnitude')
+# plt.title('sinc interpolated values')
+# plt.legend()
+# plt.show()
+#
--- simulation+interpolation/make_noise.py
+++ simulation+interpolation/make_noise.py
@@ -23,13 +23,45 @@ fs = np.array([f[index0],f[index1],f[index2],f[index3]])
 h=3
 time_delays = ind[6]
 
-# generating noise
-noise0= np.random.normal(0,10,size = len(ts[0]))
+data = np.loadtxt('../../data/RXJ1131_ALL.rdb',skiprows=2)
 
-plt.plot(ts[0]+noise0, fs[0], color='black', label = 'without noise')
-plt.plot(ts[0], fs[0], color='orange', label='without noise')
-plt.legend()
-plt.xlabel('time[days]')
-plt.ylabel('magnitude')
-# plt.legend()
-plt.show()
+def size_estimate(i,d): # i is , d is either data or dataa2
+    '''
+    inferring the size of the error relative to the magnitude in the real data
+    :param i: columns of magnitudes, for this dataset, it is either 1,3,5,7
+    :param d: dataset, ie: data
+    :return: mean of the fraction sqrt(f1^2 + f2^3+ ...)/mean(error in mag)
+    '''
+    F = d[:,i]
+    F_error = np.mean(d[:,i+1])
+    f2 = []
+    for i in range(len(f)):
+        f2.append(f[i]**2)
+    return np.int((np.sqrt(np.sum(np.array(f2))))/F_error)
+a,b,c,d = size_estimate(1,data),size_estimate(3,data),size_estimate(5,data),size_estimate(7,data)
+#print(a,b,c,d)
+
+# calculating the a th, b th, c th, d th of the power of the signal
+def std_gausian(fraction,mag):
+    f2 = []
+    for i in range(len(mag)):
+        f2.append(mag[i]**2)
+    return (np.sqrt(np.sum(np.array(f2))))/fraction
+error0,error1,error2,error3 = std_gausian(a,fs),std_gausian(b,fs),std_gausian(c,fs),std_gausian(d,fs)
+print(error0,error1,error2,error3)
+
+# generating noise
+noise0= np.random.normal(0,error0,size = len(fs[0]))
+noise1= np.random.normal(0,error1,size = len(fs[1]))
+noise2= np.random.normal(0,error2,size = len(fs[2]))
+noise3= np.random.normal(0,error3,size = len(fs[3]))
+noise = np.array([noise0,noise1,noise2,noise3])
+
+for i in range(4):
+    plt.plot(ts[i], fs[i]+noise[i], color='black', label = 'with noise')
+    plt.plot(ts[i], fs[i], color='orange', label='without noise')
+    plt.legend()
+    plt.xlabel('time[days]')
+    plt.ylabel('magnitude')
+    plt.show()
+
